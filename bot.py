67d6c2ab67bd7e58127d319a93e5fd0bb604ae00
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-10-08 08:33:02 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,48 @@
+"""
+    Shazam Telegram Bot
+    Copyright (C) 2021 @ImJanindu
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import os
+import logging
+from ShazamAPI
+from pyrogram import Client, filters, idle
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from vars import API_ID, API_HASH, BOT_TOKEN
+
+bot = Client(
+    "Shazam",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+@bot.on_message(filters.private & filters.command("shazam"))
+async def shazam(_, message):
+    if not message.reply_to_message:
+       return
+    if not message.reply_to_message.audio:
+       return
+    a = message.reply_to_message.download()
+    
+    mp3_file_content_to_recognize = open(a, 'rb').read()
+    shazam = Shazam(mp3_file_content_to_recognize)
+    recognize_generator = shazam.recognizeSong()
+    while True:
+	      await message.reply_text(next(recognize_generator))
+        os.remove(a)
+
+
+        
+bot.start()
+idle()    
